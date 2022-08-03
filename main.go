1662d7a4bf8b47c23b63cee9097d76c6e8d03980
Marvish <mschandr@uci.edu>
2022-08-03 08:54:28 -0700

    Considered all major aliens that were the most used in the series.

--- main.go
+++ main.go
@@ -2,22 +2,40 @@ package Ben10Aliens
 
 import "fmt"
 
-const all_Original_Transformations = ["Cannonbolt","Heatblast","Overflow","XLR8","Four Arms","Grey Matter","Diamondhead","Stinkfly","Rath","Humungousaur","Jetray","Goop"]
+const all_Original_Transformations = ["Cannonbolt","Heatblast","Overflow","XLR8","Four Arms","Grey Matter","Diamondhead","Stinkfly","Rath","Humungousaur","Jetray"]
 var myAlien1 string
 var myAlien2 string
 var myAlien3 string
-
+var myAlien4 string
+var myAlien5 string
+var myAlien6 string
+var myAlien7 string
+var myAlien8 string
+var myAlien9 string
+var myAlien10 string
+var myAlien11 string
 package func main() {
 	fmt.Scanln(&myAlien)
 	fmt.Println("You have decided to use" + all_Original_Transformations[0] + ", use him to dodge attacks quickly and confuse the enemy.")
 	fmt.Scanln(&myAlien2)
 	fmt.Println("You have decided to use" + all_Original_Transformations[1] + ", he incinerates his enemies through fire.")
-	
-	if myAlien := all_Original_Transformations[0]:
-		println("You have decided to use Cannonbolt, use him to dodge attacks quickly and confuse the enemy.")
-	if myAlien := all_Original_Transformations[1]:
-		println("You have decided to use Heatblast, he incinerates his enemies through fire.")
-	if myAlien := all_Original_Transformations[2]:
-		println("")
+	fmt.Scanln(&myAlien3)
+	fmt.Println("You have decided to use" + all_Original_Transformations[2] + ",he uses his cannons to release water at the enemy.")
+	fmt.Scanln(&myAlien4)
+	fmt.Println("You have decided to use" + all_Original_Transformations[3] + ", he moves very quickly to either confuse the enemy or attach them fast.")
+	fmt.Scanln(&myAlien5)
+	fmt.Println("You have decided to use" + all_Original_Transformations[4] + ", he uses his multiple arms to attack multiple enemies and make multiple attacks at once.")
+	fmt.Scanln(&myAlien6)
+	fmt.Println("You have decided to use" + all_Original_Transformations[5] + ", he uses this to become very small and think intelligently.")
+	fmt.Scanln(&myAlien7)
+	fmt.Println("You have decided to use" + all_Original_Transformations[6] + ", he uses his body to launch large blocks of diamonds to attack the enemy.")
+	fmt.Scanln(&myAlien8)
+	fmt.Println("You have decided to use" + all_Original_Transformations[7] + ", he uses his wings to release poisonous gas to release harmful toxins.")
+	fmt.Scanln(&myAlien9)
+	fmt.Println("You have decided to use" + all_Original_Transformations[8] + ", he uses his powerful body to fight enemies through enormous strength and speed.")
+	fmt.Scanln(&myAlien10)
+	fmt.Println("You have decided to use" + all_Original_Transformations[9] + ", he uses his body to lift extremely heavy objects and attack furiously.")
+	fmt.Scanln(&myAlien11)
+	fmt.Println("You have decided to use" + all_Original_Transformations[10] + ", he uses his body to fly quickly and use his eyes to shoot rays. ")
 
 }
